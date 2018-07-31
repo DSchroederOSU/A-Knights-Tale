3ba26e69c656168096b3189afca05825d8907b32
DSchroederOSU <schrodan@oregonstate.edu>
2018-07-31 11:51:44 -0700

    Bootstrap working, webpack working, header component made

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-const lodash = require('lodash');
+import _ from 'lodash';
+import 'bootstrap';
+import './scss/app.scss';
 
 function component() {
-    var element = document.createElement('div');
+    //<div class="container m-0" style="max-width: 100%;">
+    let element = document.createElement('div');
+    element.classList.add('container');
+    element.classList.add('m-0');
+    element.style.maxWidth = '100%';
+
+    let banner = document.createElement('div');
+    banner.classList.add('row');
+    banner.style.backgroundColor = '#747d8c';
+    let col1 = document.createElement('div');
+    col1.classList.add('col-md-3');
+    let col2 = document.createElement('div');
+    col2.classList.add('col-md-6');
+    col2.classList.add('p-3');
+    col2.style.textAlign = 'center';
+    let headerText = document.createElement('h1');
+    headerText.innerText="ARENA";
+    col2.appendChild(headerText);
+    let col3 = document.createElement('div');
+    col3.classList.add('col-md-3');
+
+    banner.appendChild(col1);
+    banner.appendChild(col2);
+    banner.appendChild(col3);
+    element.appendChild(banner);
 
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
     return element;
 }
 
